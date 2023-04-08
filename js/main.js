15f303513af397d7b163d535cad532c8ee6c84c2
Reza Abedi <reza.abd95@yahoo.com>
2023-04-08 08:01:06 +0430

    2nd commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openFunc(){
+    document.getElementById('container').style.cssText = 'display:none';
+    document.getElementById('cont-menu-mob').style.cssText = 'display:block';
+}
+
+function hideFunc(){
+    document.getElementById('container').style.cssText = 'display:block';
+    document.getElementById('cont-menu-mob').style.cssText = 'display:none';
+}
+
